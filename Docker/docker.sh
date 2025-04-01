2e12c711d033a3793fbec96e68065d9cc8486219
vidhyasai <relangividhyasai@gmail.com>
2025-04-01 10:59:33 +0530

    docker.sh

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo growpart /dev/nvme0n1 4
 sudo lvextend -l +50%FREE /dev/RootVG/rootVol
 sudo lvextend -l +50%FREE /dev/RootVG/varVol
@@ -19,3 +18,10 @@ sudo systemctl restart docker
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
+
+#eksctl installation`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
