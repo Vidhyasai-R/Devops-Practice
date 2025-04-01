5d084fa348d02204f9ac419bb57691dd26fa755b
vidhyasai <relangividhyasai@gmail.com>
2025-04-01 10:31:24 +0530

    docker.sh

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -6,6 +6,7 @@ sudo lvextend -l +50%FREE /dev/RootVG/varVol
 sudo xfs_growfs /
 sudo xfs_growfs /var
 
+#docker installation
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -13,3 +14,8 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 sudo systemctl restart docker
+
+#kubectl installation
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
