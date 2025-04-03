a8f71567b9dd6c08feb2bbf6817e8de1eeda1c61
vidhyasai <relangividhyasai@gmail.com>
2025-04-03 13:36:04 +0530

    docker.sh

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -28,3 +28,8 @@ sudo mv kubectl /usr/local/bin/kubectl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
+
+#kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
